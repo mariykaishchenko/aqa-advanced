672a98c7d834c281d8018ea9af804fd27c1cb2dd
Mariia Ishchenko <mariyka.ishchenko@gmail.com>
2026-05-21 00:18:01 +0100

    Home work for lesson 4

---
+++ jsBasics/task2.js
@@ -0,0 +1,5 @@
+let name1 = 'Alex';
+let name2 = 'Elis';
+
+console.log('Hello ' + name1 + ' and ' + name2);
+console.log(`Hello ${name1} and ${name2}`);
---
+++ jsBasics/task4.3.js
@@ -0,0 +1,5 @@
+let radius = 10;
+let height = 30;
+let volume = Math.PI * Math.pow(radius, 2) * height;
+
+console.log(volume.toFixed(2));
