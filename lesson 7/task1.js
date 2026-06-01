ddea7d78df1a06aee0367a5c85f3743ef4c65dcb
Mariia Ishchenko <mariyka.ishchenko@gmail.com>
2026-06-01 15:03:19 +0100

    Home work lesson 7

---
+++ lesson 7/task1.js
@@ -0,0 +1,18 @@
+function handleNum(num, evenCallback, oddCallback) {
+    if (num % 2 === 0) {
+        evenCallback();
+    } else {
+        oddCallback();
+    }
+}
+
+function handleEven() {
+    console.log("number is even");
+}
+
+function handleOdd() {
+    console.log("number is odd");
+}
+
+handleNum(10, handleEven, handleOdd);
+handleNum(5, handleEven, handleOdd);
