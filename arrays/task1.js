2e46d457887dd31514e03d6d62e73c09ab77e719
Mariia Ishchenko <mariyka.ishchenko@gmail.com>
2026-06-04 16:46:14 +0100

    Home work lesson 8

---
+++ arrays/task1.js
@@ -0,0 +1,18 @@
+const numbers = [2, -5, 0, 7, -3, 0, 10, -8, 11];
+let positiveCount = 0;
+let negativeCount = 0;
+let zeroCount = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 0) {
+        zeroCount++;
+    } else if (numbers[i] > 0) {
+        positiveCount++;
+    } else if (numbers[i] < 0) {
+        negativeCount++;
+    }
+}
+
+console.log("Positive numers count", positiveCount);
+console.log("Negative numbers count", negativeCount);
+console.log("Zero numbers count", zeroCount);
---
+++ arrays/task6.js
@@ -0,0 +1,8 @@
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+const copyList = numbersList.slice();
+copyList.sort((a, b) => {
+    return a - b;
+});
+
+console.log(numbersList);
+console.log(copyList);
